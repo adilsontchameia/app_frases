291d625774ff1cfb29693fc3cf21856f5b01a787
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-14 15:38:48 +0100

    Criacao da interface completa do app

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 }
 
 //Segundo criar um StatefulWidget
-//Widgets que podem ser alterados (variaveis)
+//Widgets que podem ser alterados, dinamicos (variaveis)
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -21,11 +21,40 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     //Terceiro retornar um scaffold, para a base do app.
     return Scaffold(
-      appBar: AppBar(
-        title: Text("App Frase do Dia"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("App Frase do Dia"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            //width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("images/logo.png"),
+                Text(
+                  "Clique abaixo para gerar uma frase !",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 25,
+                      fontStyle: FontStyle.italic, color: Colors.blue),
+                ),
+                RaisedButton(
+                  child: Text(
+                    "Nova Frase",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
